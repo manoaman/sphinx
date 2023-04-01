04d00121d6c0aceea42df38d186a3c51acbe9cbd
Seita Yamashita <manoaman@gmail.com>
2023-03-31 18:16:37 -0700

    update

--- conf.py
+++ conf.py
@@ -54,4 +54,4 @@ html_theme = 'alabaster'
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
 html_static_path = ['_static', 'css']
-html_css_files = ['/docs/custom.css']
+html_css_files = ['docs/_static/custom.css']
